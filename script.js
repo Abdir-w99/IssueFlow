18be16908cce6366c2553de5e39ef446163f1f28
AbdirahmanWarsame <abdir.w99@gmail.com>
2026-03-30 20:48:36 +0200

    Connect HTML elements to JavaScript

---
+++ script.js
@@ -0,0 +1,7 @@
+const issueForm = document.getElementById("issue-form");
+const titleInput = document.getElementById("title");
+const descriptionInput = document.getElementById("description");
+const prioritySelect = document.getElementById("priority");
+const issueList = document.getElementById("issue-list");
+
+const issues = [];
